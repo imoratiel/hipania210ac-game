3ec0f998a1a9c34068467b304d640a34b4e8b538
MrNachoM <imoratiel@gmail.com>
2026-03-24 17:48:06 +0100

    fix: cliente en puerto 80

--- server/src/services/OAuthService.js
+++ server/src/services/OAuthService.js
@@ -108,7 +108,7 @@ async function handleGoogleCallback(req, res) {
 
         res.cookie('access_token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: process.env.COOKIE_SECURE === 'true',
             sameSite: 'lax',
             maxAge: 24 * 60 * 60 * 1000
         });
